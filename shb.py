efa29f134a7a1e0925bc2dfab9b722c5a86dd6f1
shibin03 <116878972+shibin03@users.noreply.github.com>
2022-12-08 21:49:25 +0530

    Add files via upload
    Initiate comment

---
+++ shb.py
@@ -0,0 +1,8 @@
+def area(radius):
+    return pi*(radius**2)
+def circumference(radius):
+    return 2*pi*radius
+def sphereSurface(radius):
+    return 4.0*area(radius)
+def sphereVolume(radius):
+    return (4.0/3.0)*pi*(radius**3)
